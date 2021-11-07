1e1304e1b5fcec6aafe326865567ad1c19e83704
Michael Hamill <michael.hamill3@gmail.com>
2021-11-07 15:41:26 -0500

    Implemented successful registration reroute to the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import store from './state/store';
 
 import './scss/index.scss';
 
 import Navbar from './components/Navbar/Navbar';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+import Dashboard from './pages/Dashboard/Dashboard';
 import Landing from './pages/Landing/Landing';
 import Login from './pages/Auth/Login/Login';
 import Register from './pages/Auth/Register/Register';
-import Dashboard from './pages/Dashboard/Dashboard';
 
 const App = () => {
   return (
@@ -21,7 +24,7 @@ const App = () => {
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
           </Switch>
         </Fragment>
       </Router>
