b7505001b5438fd21d82a56136f080bc1b51806b
Michael Hamill <michael.hamill3@gmail.com>
2021-11-22 07:46:20 -0500

    Updated landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -14,7 +14,7 @@ const Landing = (props) => {
   }, [authenticated, props.history]);
 
   return (
-    <div className="relative bg-white overflow-hidden">
+    <div className="relative bg-white overflow-hidden mb-2">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
@@ -43,8 +43,8 @@ const Landing = (props) => {
           </main>
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0">
-        <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={landingImage} alt="" />
+      <div className="flex justify-center lg:absolute lg:inset-y-0 lg:right-0">
+        <img className="h-56 w-11/12 object-cover rounded-lg lg:rounded-none sm:h-72 md:h-96 lg:w-full lg:h-full" src={landingImage} alt="" />
       </div>
     </div>
   );
