608f81d37787e104ba55057b72cca1a4feed830c
Michael Hamill <michael.hamill3@gmail.com>
2021-11-22 20:07:44 -0500

    Added default font

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,5 @@ import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import './index.css';
 import App from './App';
+<link href="https://fonts.googleapis.com/css?family=Open Sans" rel="stylesheet"></link>;
 ReactDOM.render(<App />, document.getElementById('root'));
