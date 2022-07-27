782b35416b7f94e95f6ff61baeaebec2993bcc90
Michael Hamill <michael.hamill3@gmail.com>
2022-07-26 22:17:45 -0400

    Added createdAt timestamp to message model

--- models/message.js
+++ models/message.js
@@ -1,32 +1,37 @@
 const { Schema, model } = require('mongoose');
 
-const messageSchema = new Schema({
-  sender: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  receiver: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  body: {
-    type: Object,
-    text: {
-      type: String,
-      trim: true
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
     },
-    attachments: [
-      {
-        type: Buffer
+    receiver: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    body: {
+      type: Object,
+      text: {
+        type: String,
+        trim: true
+      },
+      attachments: [
+        {
+          type: Buffer
+        }
+      ],
+      validate() {
+        return this.text || (this.attachments && this.attachments.length > 0);
       }
-    ],
-    validate() {
-      return this.text || (this.attachments && this.attachments.length > 0);
     }
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false }
   }
-});
+);
 
 const Message = model('Message', messageSchema);
 module.exports = Message;
